4937278b10bd776885d66bf1d20372e92741c1b3
Niko Tzikas <niko.tzikas@gmail.com>
2017-01-18 16:58:40 -0500

    stor

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ var YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/search';
 var KEY = 'AIzaSyA3IHL73MF00WFjgxdwzg57nI1CwW4dybQ';
 var USERSEARCHTERM = '';
 
+$(function(){
+USERSEARCHTERM = window.localStorage.getItem('search');
+if(USERSEARCHTERM){
+	getDataFromApi(USERSEARCHTERM, displayYoutubeSearchData); 
+}
+
+})
 function getDataFromApi(searchTerm, callback) {
 	var query = {
 		part: 'snippet',
@@ -13,43 +20,24 @@ function getDataFromApi(searchTerm, callback) {
 	$.getJSON(YOUTUBE_URL, query, callback);
 }
 
-
-
-
-var thisIsDefNotTheRightWayToDoThisButJustAHint; 
-var loadNext = setInterval(function(){
-	var fakeQuery = {
-		part: 'snippet',
-		key: KEY,
-		maxResults: 6,
-		type: 'video',
-		q: 'dog',
-		pageToken:thisIsDefNotTheRightWayToDoThisButJustAHint,
-	}
-
-
-	console.log($.getJSON(YOUTUBE_URL, fakeQuery, displayYoutubeSearchData))
-},3000)
-
-
-
-alert('lets look up dogs. press space to stop ');
-$(document).keyup(function(evt) {
-	if (evt.keyCode == 32) {
-		clearInterval(loadNext)
-		alert('sit puppy!');
-	
-	}
-})
-
-
-
 function getNextPageData(searchTerm, nextPage, callback) {
 	var query = {
 		part: 'snippet',
 		key: KEY,
 		maxResults: 6,
-		nextPageToken: nextPage,
+		pageToken: nextPage,
+		type: 'video',
+		q: searchTerm
+	}
+	$.getJSON(YOUTUBE_URL, query, callback);
+}
+
+function getPrevPageData(searchTerm, nextPage, callback) {
+	var query = {
+		part: 'snippet',
+		key: KEY,
+		maxResults: 6,
+		pageToken: nextPage,
 		type: 'video',
 		q: searchTerm
 	}
@@ -57,39 +45,47 @@ function getNextPageData(searchTerm, nextPage, callback) {
 }
 
 function displayYoutubeSearchData(data) {
-	console.log(data);
 	var resultsHTML = '<div class="row">';
 	data.items.forEach(function (item) {
 		resultsHTML = (resultsHTML + '<div class="search-item col s12 m6"><div class="card small hoverable">' +
-			'<div class="card-image">' +
-			'<iframe src="https://www.youtube.com/embed/' + item.id.videoId + '" frameborder="0" allowfullscreen></iframe>' +
-			'</div>' +
-			'<div class="card-content">' +
-			'<p class="truncate">' + item.snippet.title + '</p>' +
-			'</div>' +
-			'<div class="card-action">' +
-			'<a href="https://www.youtube.com/channel/' + item.snippet.channelId + '">View more from '+ item.snippet.channelTitle + '</a>' +
-			'</div>' + 
-			'</div></div>')
+				'<div class="card-image">' +
+				'<iframe src="https://www.youtube.com/embed/' + item.id.videoId + '" frameborder="0" allowfullscreen></iframe>' +
+				'</div>' +
+				'<div class="card-content">' +
+				'<p class="truncate">' + item.snippet.title + '</p>' +
+				'</div>' +
+				'<div class="card-action">' +
+				'<a href="https://www.youtube.com/channel/' + item.snippet.channelId + '">View more from '+ item.snippet.channelTitle + '</a>' +
+				'</div>' + 
+				'</div></div>')
 	});
 
 	resultsHTML = resultsHTML + '</div>';
 
 	$('.search-results').html(resultsHTML);
-	
+
 	console.log(data.nextPageToken);
 	$('.next-page').removeClass('hidden');
 	$('.next-page').click(function(e) {
 		e.preventDefault();
 		getNextPageData(USERSEARCHTERM, data.nextPageToken, displayYoutubeSearchData);
+		$('.prev-page').removeClass('hidden');
+		$('.prev-page').click(function(e) {
+			e.preventDefault();
+			getNextPageData(USERSEARCHTERM, data.prevPageToken, displayYoutubeSearchData);
+		});
 	});
-	thisIsDefNotTheRightWayToDoThisButJustAHint = data.nextPageToken;
+
+
 }
 
 function watchSubmit() {
 	$('form[name="user-search"]').submit(function(e) {
 		e.preventDefault();
 		USERSEARCHTERM = $(this).find('.user-search-input').val();
+		window.localStorage.setItem('search',USERSEARCHTERM);
+		console.log(window.localStorage);
+		console.log(window.localStorage.getItem('search'))
 		getDataFromApi(USERSEARCHTERM, displayYoutubeSearchData); 
 	});
 }
@@ -97,3 +93,4 @@ function watchSubmit() {
 $(document).ready(function () {
 	watchSubmit();
 })
+
